f1d6eb7d2f2446eeb40ab6518a4ef27cf11c1f47
rbhavitha <bhavitharedrouthu@gmail.com>
2020-10-14 03:26:04 +0530

    Email-UC5

--- emailValidation.sh
+++ emailValidation.sh
@@ -4,7 +4,7 @@ echo "Validation Of Email Address"
 
 read -p "enter email: " email
 
-email_Pattern="^[0-9a-zA-z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}$"
+email_Pattern="^[0-9a-zA-z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})$"
 if [[ $email =~ $email_Pattern ]]
 then
         echo "Valid"
