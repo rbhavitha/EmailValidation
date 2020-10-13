45c9ef786ca82c98b5143781354ae05fd0255be9
rbhavitha <bhavitharedrouthu@gmail.com>
2020-10-14 03:01:04 +0530

    Email-UC1

--- emailValidation.sh
+++ emailValidation.sh
@@ -1,3 +1,14 @@
 #!/bin/bash -x
 
 echo "Validation Of Email Address"
+
+read -p "enter email: " email
+
+#start with abc
+email_Pattern="^[0-9a-zA-z]*$"
+if [[ $email =~ $email_Pattern ]]
+then
+        echo "Valid"
+else
+        echo "Invalid"
+fi
